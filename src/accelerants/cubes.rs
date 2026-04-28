022a33cb2468398f16314b0f10d4f4a269a9afa8
nrposner <nicolasposner@gmail.com>
2026-04-28 09:56:54 -0400

    polishing orba_ecc_helper

--- src/accelerants/cubes.rs
+++ src/accelerants/cubes.rs
@@ -2,7 +2,6 @@
 //!
 
 use pyo3::prelude::*;
-use crate::accelerants::G_SI;
 
 use std::f64::consts::PI;
 
@@ -11,6 +10,7 @@ use std::f64::consts::PI;
 
 /// A Rust implementation of a dedicated cubic root solver for McFACTS
 #[pyfunction(signature = (x0, y0))]
+#[inline(always)]
 pub fn cubic_y_root_cardano(x0: f64, y0: f64) -> ([f64; 3], usize) {
     if x0 == 0.0 {
         ([y0/1.5, 0.0, 0.0], 1) //padding out the array
@@ -40,6 +40,7 @@ pub fn cubic_y_root_cardano(x0: f64, y0: f64) -> ([f64; 3], usize) {
 }
 
 #[pyfunction(signature=(x0, y0, omega_s))] 
+#[inline(always)]
 pub fn cubic_finite_step_root_cardano(
     x0: f64,
     y0: f64,
@@ -102,19 +103,25 @@ pub fn cubic_finite_step_root_cardano(
     (buffer, pair_count)
 }
 
-fn components_from_el(e: f64, l:f64, units: Option<&str>, smbh_mass: f64) -> (f64, f64) {
+#[inline(always)]
+fn components_from_el(
+    e: f64, 
+    l:f64, 
+    // units: Option<&str>, 
+    smbh_mass: f64
+) -> (f64, f64) {
 
-    let g_val = if units == Some("geometric") {
-        G_SI
-    } else {
-        1.0
-    };
-
-    let g_times_mass = g_val * smbh_mass;
+    // let g_val = if units == Some("geometric") {
+    //     G_SI
+    // } else {
+    //     1.0
+    // };
+    //
+    // let g_times_mass = g_val * smbh_mass;
     
-    let orb_a = - g_times_mass/(2.0*e);
+    let orb_a = - smbh_mass/(2.0*e);
 
-    let mut one_minus_ecc2_sqrt = l/(g_times_mass * orb_a).sqrt();
+    let mut one_minus_ecc2_sqrt = l/(smbh_mass * orb_a).sqrt();
 
     one_minus_ecc2_sqrt = if (one_minus_ecc2_sqrt - 1.0 > 0.0) && (one_minus_ecc2_sqrt - 1.0 < 1e-2) {
         1.0-1e-2
@@ -131,7 +138,7 @@ fn components_from_el(e: f64, l:f64, units: Option<&str>, smbh_mass: f64) -> (f6
     (orb_a / (2.0 * smbh_mass), ecc)
 }
 
-#[pyfunction(signature = (e1, l1, e2, l2, delta_e, m1, m2, units = None, smbh_mass = None))]
+#[inline(always)]
 pub fn transition_physical_as_el(
     e1: f64, 
     l1: f64,
@@ -140,19 +147,18 @@ pub fn transition_physical_as_el(
     delta_e: f64,
     m1: f64,
     m2: f64,
-    units: Option<&str>, // defaults to 'geometric'
+    // units: Option<&str>, // defaults to 'geometric'
     smbh_mass: Option<f64>, // defaults to 10e8
 ) -> (f64, f64, f64, f64) {
 
-    let units = units.unwrap_or("geometric");
+    // let units = units.unwrap_or("geometric");
+    // let g_val = if units == "geometric" {
+    //     1.0
+    // } else {
+    //     G_SI
+    // };
 
     let smbh_mass = smbh_mass.unwrap_or(10.0e8);
-
-    let g_val = if units == "geometric" {
-        1.0
-    } else {
-        G_SI
-    };
 
     // Assume consistent units SI only
     let eps1 = e1/m1;
@@ -162,12 +168,13 @@ pub fn transition_physical_as_el(
 
     // find omega_0 scale, which is based on the acceptor (2) non-eccentric object. This means ell0 = ell2
     let ell0 = ell2;
-    let g_times_mass = g_val * smbh_mass;
-    let omega_0 = g_times_mass.powi(2) / ell0.powi(3);
+    // let g_times_mass = g_val * smbh_mass;
+    // let omega_0 = g_times_mass.powi(2) / ell0.powi(3);
+    let omega_0 = smbh_mass.powi(2) / ell0.powi(3);
     let eps0 = ell0 * omega_0;
 
     let base = -2.0 * eps2;
-    let omega_2 = base.powf(1.5) / g_times_mass;
+    let omega_2 = base.powf(1.5) / smbh_mass;
     // let omega_2 = (-2.0 * eps2).powi(3) / (g_times_mass);
 
     let x0 = eps2/eps0;
@@ -226,7 +233,7 @@ pub fn transition_physical_as_el(
         let mut min_omega = f64::INFINITY;
         for r in roots_array.iter().take(roots_count) {
             let ell = r * ell0;
-            let omega = g_times_mass.powi(2) / ell.powi(3);
+            let omega = smbh_mass.powi(2) / ell.powi(3);
             if omega > 0.0 && omega < min_omega {
                 min_omega = omega;
             }
@@ -239,7 +246,7 @@ pub fn transition_physical_as_el(
         let mut min_omega_alt = f64::INFINITY;
         for r in roots_alt_array.iter().take(roots_alt_count) {
             let ell = r * ell0;
-            let omega = g_times_mass.powi(2) / (ell).powi(3);
+            let omega = smbh_mass.powi(2) / (ell).powi(3);
             if omega > 0.0 && omega < min_omega_alt {
                 min_omega_alt = omega;
             }
@@ -254,7 +261,7 @@ pub fn transition_physical_as_el(
 
 /// Calculate new orb_a and ecc values for two objects that dynamically interact
 #[pyfunction(signature=(smbh_mass, orb_a_give, orb_a_take, mass_give, mass_take, ecc_give, ecc_take, radius_give, radius_take, id_num_give, id_num_take, delta_energy_strong, flag_obj_types))]
-pub fn encounters_new_orba_ecc(
+pub fn encounters_new_orba_ecc_helper(
     smbh_mass: f64,
     orb_a_give: f64,
     orb_a_take: f64,
@@ -262,13 +269,12 @@ pub fn encounters_new_orba_ecc(
     mass_take: f64,
     ecc_give: f64,
     ecc_take: f64,
-    radius_give: f64,
+    radius_give: Option<f64>,
     radius_take: f64,
     id_num_give: u32,
     id_num_take: u32,
     delta_energy_strong: f64,
-    flag_obj_types: u32, // should be u64 for memory layout?
-
+    flag_obj_types: i32, // most of it not used
 ) -> (f64, f64, f64, f64, Option<u32>, Option<u32>) {
     let smbh_mass_geometric = 1.0;
     let mass_scale = smbh_mass / 1.0; // huh????
@@ -279,7 +285,7 @@ pub fn encounters_new_orba_ecc(
 
     let (v_relative, v_esc_sq) = match flag_obj_types {
         0 => {
-            let radius_give_geometric = radius_give * 2.0 * smbh_mass_geometric;
+            let radius_give_geometric = radius_give.unwrap() * 2.0 * smbh_mass_geometric;
             let radius_take_geometric = radius_take * 2.0 * smbh_mass_geometric;
             let v_relative = (smbh_mass_geometric / orb_a_give_geometric).sqrt() - (smbh_mass_geometric / orb_a_take_geometric).sqrt();
             let v_esc_sq = smbh_mass_geometric / (radius_give_geometric.max(radius_take_geometric));
@@ -304,19 +310,16 @@ pub fn encounters_new_orba_ecc(
     let mu_geometric = mass_give_geometric * mass_take_geometric / (mass_give_geometric + mass_take_geometric);
     let delta_e = delta_energy_strong * mu_geometric * (1.0 / ((1.0 / v_relative.powi(2)) + (1.0 / v_esc_sq)));
 
-    // unnecessary declarations
-    //
-    // let id_num_unbound: Option<u32> = None;
-    // let id_num_flipped_rotation: Option<u32> = None;
-
-    let (e_give_final, e_take_final, j_give_final, j_take_final) = transition_physical_as_el(e_give_initial, j_give_initial, e_take_initial, j_take_initial, delta_e, mass_give_geometric, mass_take_geometric, None, Some(smbh_mass_geometric));
-
+    // at present don't use one of the arguments, commenting out
+    // let (e_give_final, e_take_final, j_give_final, j_take_final) = transition_physical_as_el(e_give_initial, j_give_initial, e_take_initial, j_take_initial, delta_e, mass_give_geometric, mass_take_geometric, None, Some(smbh_mass_geometric));
+    let (e_give_final, e_take_final, j_give_final, j_take_final) = transition_physical_as_el(e_give_initial, j_give_initial, e_take_initial, j_take_initial, delta_e, mass_give_geometric, mass_take_geometric, Some(smbh_mass_geometric));
 
     let (orb_a_give_final, orb_a_take_final, ecc_give_final, ecc_take_final, id_num_unbound) = if e_give_initial + delta_e > 0.0 {
         let orb_a_give_final = orb_a_give;
         let ecc_give_final = ecc_give;
         let id_num_unbound = id_num_give;
-        let (orb_a_take_final, ecc_take_final) = components_from_el(e_take_final / mass_take_geometric, j_take_final / mass_take_geometric, None, smbh_mass_geometric);
+        // removing unnecessary None argument
+        let (orb_a_take_final, ecc_take_final) = components_from_el(e_take_final / mass_take_geometric, j_take_final / mass_take_geometric, smbh_mass_geometric);
 
         (orb_a_give_final, orb_a_take_final, ecc_give_final, ecc_take_final, Some(id_num_unbound))
 
@@ -324,13 +327,13 @@ pub fn encounters_new_orba_ecc(
         let orb_a_take_final = orb_a_take;
         let ecc_take_final = ecc_take;
         let id_num_unbound = id_num_take;
-        let (orb_a_give_final, ecc_give_final) = components_from_el(e_give_final / mass_give_geometric, j_give_final / mass_give_geometric, None, smbh_mass_geometric);
+        let (orb_a_give_final, ecc_give_final) = components_from_el(e_give_final / mass_give_geometric, j_give_final / mass_give_geometric, smbh_mass_geometric);
 
         (orb_a_give_final, orb_a_take_final, ecc_give_final, ecc_take_final, Some(id_num_unbound))
 
     } else {
-        let (orb_a_give_final, ecc_give_final) = components_from_el(e_give_final / mass_give_geometric, j_give_final / mass_give_geometric, None, smbh_mass_geometric);
-        let (orb_a_take_final, ecc_take_final) = components_from_el(e_take_final / mass_take_geometric, j_take_final / mass_take_geometric, None, smbh_mass_geometric);
+        let (orb_a_give_final, ecc_give_final) = components_from_el(e_give_final / mass_give_geometric, j_give_final / mass_give_geometric, smbh_mass_geometric);
+        let (orb_a_take_final, ecc_take_final) = components_from_el(e_take_final / mass_take_geometric, j_take_final / mass_take_geometric, smbh_mass_geometric);
 
         (orb_a_give_final, orb_a_take_final, ecc_give_final, ecc_take_final, None)
     };
