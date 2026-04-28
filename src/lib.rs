b2a163709f25d010c1a6eed3fc9a30b61d0da362
nrposner <nicolasposner@gmail.com>
2026-04-28 10:01:22 -0400

    export orba_ecc_helper

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ mod accelerants;
 
 use accelerants::{
     baruteau::baruteau_helper,
-    cubes::{encounters_new_orba_ecc, cubic_y_root_cardano, cubic_finite_step_root_cardano, transition_physical_as_el},
+    cubes::{encounters_new_orba_ecc_helper, cubic_y_root_cardano, cubic_finite_step_root_cardano},
     evolution::evolution_helper,
     powerlaw::{generate_r, sample_powerlaw_icdf},
     tau::{tau_ecc_dyn_helper, tau_inc_dyn_helper},
@@ -24,13 +24,12 @@ use tools::merge_tree::MergeForest;
 #[pymodule]
 fn mcfast(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(baruteau_helper, m)?)?;
-    m.add_function(wrap_pyfunction!(encounters_new_orba_ecc, m)?)?;
+    m.add_function(wrap_pyfunction!(encounters_new_orba_ecc_helper, m)?)?;
     m.add_function(wrap_pyfunction!(evolution_helper, m)?)?;
     m.add_function(wrap_pyfunction!(cubic_finite_step_root_cardano, m)?)?;
     m.add_function(wrap_pyfunction!(analytical_kick_velocity_helper, m)?)?;
     m.add_function(wrap_pyfunction!(merged_orb_ecc_helper, m)?)?;
     m.add_function(wrap_pyfunction!(cubic_y_root_cardano, m)?)?;
-    m.add_function(wrap_pyfunction!(transition_physical_as_el, m)?)?;
     m.add_function(wrap_pyfunction!(generate_r, m)?)?;
     m.add_function(wrap_pyfunction!(sample_powerlaw_icdf, m)?)?;
     m.add_function(wrap_pyfunction!(tau_ecc_dyn_helper, m)?)?;
